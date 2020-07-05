7a04db1c420b0985d6dbf9060bbbee1651071eb1
macOS <macos@hoangquan.local>
2020-07-05 12:38:08 +0700

    new port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const path = require('path')
-const port = 80
+const port = 8080
 
 app.use(express.static(__dirname + '/dist/framertools'))
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'dist/framertools/index.html')))
