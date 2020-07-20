26cc82fc18bd75a05ef090f86a5d325634be71f0
Hoàng Văn Quân <quanlybanthan@gmail.com>
2020-07-20 02:11:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http')
 const path = require('path')
 const port = 8080
 
-app.use(express.static(__dirname + '/dist/framertools'))
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'dist/framertools/index.html')))
+app.use(express.static(__dirname + '/dist'))
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
 const server = http.createServer(app)
 server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
